a7b2bf4df3bf165549778fccbb859bc2ef249cf8
Matthew Johnson <JohnsonM20@lsrhs.net>
2019-09-17 11:43:22 -0400

    Initial Commit

---
+++ FirstQuizGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirstQuizGame
+//
+//  Created by Matthew Johnson on 9/17/19.
+//  Copyright © 2019 Matthew Johnson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
